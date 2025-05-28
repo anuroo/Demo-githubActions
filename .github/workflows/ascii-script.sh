f8d07b7703b3329a05295eda9dd7ba59c59b1627
anuroo <anuroop72@gmail.com>
2025-05-28 18:42:57 +0530

    Modified Shell script

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,6 @@
+#/bin/sh
 sudo apt-get install cowsay -y
-owsay -f dragon "run for cover,I am a dragon..." >> dragon.txt
+cowsay -f dragon "run for cover,I am a dragon..." >> dragon.txt
 grep -i "dragon" dragon.txt 
 cat dragon.txt
 ls -ltra
